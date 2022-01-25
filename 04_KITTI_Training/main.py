e7f73440690aea4dbdb1c0a817a84a8c28e15b55
owl-d <limdoyou000921@gmail.com>
2022-01-25 17:33:35 +0900

    04

--- 04_KITTI_Training/main.py
+++ 04_KITTI_Training/main.py
@@ -19,27 +19,26 @@ kitti_dataset_validation = KITTI_dataset(dataset_path='KITTI_dataset_info.hdf5',
 kitti_dataset_test = KITTI_dataset(dataset_path='KITTI_dataset_info.hdf5', mode='test')
 
 #데이터로더
-training_dataloader = DataLoader(dataset=kitti_dataset_training, batch_size=1, shuffle=True, num_workers=0)
-validation_dataloader = DataLoader(dataset=kitti_dataset_validation, batch_size=1, shuffle=True,num_workers=0)
-test_dataloader = DataLoader(dataset=kitti_dataset_test, batch_size=1, shuffle=True, num_workers=0)
+training_dataloader = DataLoader(dataset=kitti_dataset_training, batch_size=1, shuffle=False, num_workers=0)
+validation_dataloader = DataLoader(dataset=kitti_dataset_validation, batch_size=1, shuffle=False,num_workers=0)
+test_dataloader = DataLoader(dataset=kitti_dataset_test, batch_size=1, shuffle=False, num_workers=0)
 
 
 # if __name__ == '__main__':
 for i_batch, sample_batched in enumerate(training_dataloader):
 
-    print(sample_batched)
     # 6DoF 출력
+    print('batch', i_batch, end=' ')
     print("pose(6DoF) : \n x : {}, y : {}, z : {} \n roll : {}, pitch : {}, yaw : {}"
                 .format(sample_batched['x'], sample_batched['y'], sample_batched['z'],
                 sample_batched['roll'], sample_batched['pitch'], sample_batched['yaw']))
 
     # 해당 이미지 출력
     idx_img_path = sample_batched['path']
-    print(idx_img_path)
-    # idx_img = cv.imread(idx_img_path, cv.IMREAD_COLOR)
-    # cv.imshow('idx_img', idx_img)
-    # cv.waitKey(0)
-    # cv.destroyAllWindows()
+    idx_img = cv.imread(str(idx_img_path[0]), cv.IMREAD_COLOR)
+    cv.imshow('idx_img', idx_img)
+    cv.waitKey(0)
+    cv.destroyAllWindows()
 
     # 처음 두 개 샘플만 보여주기
     if i_batch == 1:
