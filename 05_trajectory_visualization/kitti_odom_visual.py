0c6d436d554d25138fdea44aaf7c5fc552f2de90
owl-d <limdoyou000921@gmail.com>
2022-02-26 17:08:58 +0900

    up

--- 05_trajectory_visualization/kitti_odom_visual.py
+++ 05_trajectory_visualization/kitti_odom_visual.py
@@ -1,18 +1,20 @@
 import matplotlib.pyplot as plt
 
 
+x_list = []
+y_list = []
+
 def process(line):
                 
-    ### transformation matix 로 pose 계산 : 6DoF ##############################################################
+    ### transformation matix 로 궤적 얻기 ##############################################################
     pose = line.strip().split()
 
-    x = pose[3] #position
-    y = pose[7]
-    ##########################################################################################################
+    x_list.append(float(pose[3]))  # position
+    y_list.append(float(pose[11]))
 
-    plt.plot(x, y, 'ob')
+    ##################################################################################################
 
-file_path = '/media/doyu_now/Linux Workspace/ICSL_Project/Visual SLAM/KITTI_data_odometry_color/data_odometry_poses/dataset/poses/04.txt'
+file_path = '/media/doyu_now/Linux Workspace/ICSL_Project/Visual SLAM/KITTI_data_odometry_color/data_odometry_poses/dataset/poses/06.txt'
 with open(file_path, 'r') as f:
     while True:
 
@@ -23,6 +25,7 @@ with open(file_path, 'r') as f:
         
         process(line)
 
+plt.plot(x_list, y_list, 'ob')
 
 plt.title('kitti odom - scenario 04')
 plt.show()
--- 05_trajectory_visualization/rtabmap_odom_visual.py
+++ 05_trajectory_visualization/rtabmap_odom_visual.py
@@ -1,23 +1,28 @@
 import matplotlib.pyplot as plt
 
+position_x = []
+position_y = []
+position_z = []
+
+orientation_x = []
+orientation_y = []
+orientation_z = []
+orientation_w = []
 
 def process(line):
                 
     ### transformation matix 로 pose 얻기 #########################################################
     pose = line.strip().split(",")
 
-    position_x = pose[5] #position
-    position_y = pose[6]
-    position_z = pose[7]
-    orientation_x = pose[8] #orientation
-    orientation_y = pose[9]
-    orientation_z = pose[10]
-    orientation_w = pose[11]
+    position_x.append(float(pose[5]))       # position
+    position_y.append(float(pose[6]))
+    position_z.append(float(pose[7]))
+    
+    orientation_x.append(float(pose[8]))    # orientation
+    orientation_y.append(float(pose[9]))
+    orientation_z.append(float(pose[10]))
+    orientation_w.append(float(pose[11]))
     ##############################################################################################
-
-    plt.plot(position_x, position_y, 'or')
-
-
 
 idx = 0
 with open("/home/doyu_now/04_odom_data.txt", 'r') as f:
@@ -36,5 +41,9 @@ with open("/home/doyu_now/04_odom_data.txt", 'r') as f:
             idx += 1
 
 
+
 plt.title('rtabmap odom - scenario 04')
+
+plt.plot(position_y, position_x, 'or')
+
 plt.show()
